25ee627af7f035a26e3effd04633bdd2a5c2e4f1
mesgnawoldeab <96284818+Mesgna616@users.noreply.github.com>
2022-05-21 14:36:49 +0100

    Update scrpit.js

--- scrpit.js
+++ scrpit.js
@@ -28,3 +28,34 @@ document.getElementById('button').addEventListener('click', (event) => {
   }
 
 });
+
+
+var animal, adejctive, verb, noun;
+
+function randomInt(n) {
+  // Return a random number from in [0, n[
+  return Math.floor(Math.random()*n);
+}
+
+function randomMember(arr) {
+  // Return a random member of the array
+  return arr[randomInt(arr.length)]
+}
+
+
+animal = ['Frog', 'Camel', 'Dog'];
+let element_animal = document.getElementById('animal');
+element_animal.innerText = randomMember(animal);
+
+adejctive = ['Green', 'Scary', 'Big'];
+let element_adjective = document.getElementById('adjective');
+element_adjective.innerText = randomMember(adejctive);
+
+verb = ['jump', 'kick', 'pick'];
+let element_verb = document.getElementById('verb');
+element_verb.innerText = randomMember(verb);
+
+noun = ['girl', 'boy', 'woman'];
+let element_noun = document.getElementById('noun');
+element_noun.innerText = randomMember(noun);
+
